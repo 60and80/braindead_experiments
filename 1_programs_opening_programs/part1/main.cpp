a583fb7f35077d84eea0942410ad5240b4451ce6
Hugh Marshall <hugh.m@me.com>
2020-11-06 10:00:31 +1300

    Simplified code a lot

--- 1_programs_opening_programs/part1/main.cpp
+++ 1_programs_opening_programs/part1/main.cpp
@@ -6,22 +6,5 @@
 int main() {
     std::cout << "MAIN\n";
 
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-
-    ZeroMemory( &si, sizeof(si) );
-    si.cb = sizeof(si);
-    ZeroMemory( &pi, sizeof(pi) );
-
-    CreateProcess( nullptr,   // Runs second program!
-                   const_cast<char *>("part1_auxiliary"),
-                   nullptr,
-                   nullptr,
-                   FALSE,
-                   0,
-                   nullptr,
-                   nullptr,
-                   &si,
-                   &pi );
-
+    system("part1_auxiliary");
 }
